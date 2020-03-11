5e9a4144e90824aebc81b1b7e8b4b7337c17be69
baschenbrenner <baschenbrenner@hotmail.com>
2020-03-11 19:46:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let main = document.querySelector("#main")
+main.remove()
+
+let body = document.querySelector("body")
+
+body.innerHTML  += "<h1 id='victory'>Ben is the champion</h1>"
+let newHeader = document.querySelector("h1#victory")
